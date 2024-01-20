4dcd0b8fe6812a67157094fbb7db73a7dca8028b
AmarnathKarthik286 <155959716+AmarnathKarthik286@users.noreply.github.com>
2024-01-20 20:53:09 +0530

    Create graph.py

---
+++ codes/graph.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+
+def geometric_progression(n, first_term, common_ratio):
+    terms = [first_term * (common_ratio ** i) for i in range(n)]
+    return terms
+
+# Parameters
+n_terms = 8  # Number of terms in the progression (changed to 7)
+first_term = -3
+common_ratio = -3
+
+# Generate terms
+terms = geometric_progression(n_terms, first_term, common_ratio)
+
+# Plotting
+plt.plot(range(1, n_terms + 1), terms, marker='o', linestyle='-', color='b')
+plt.title('Geometric Progression: First Term = -3, Common Ratio = -3')
+plt.xlabel('Term')
+plt.ylabel('Value')
+plt.grid(True)
+plt.show()
+plt.savefig
